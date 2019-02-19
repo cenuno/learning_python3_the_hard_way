27dd50c9fcb5011b70c663442e8faee1c637a395
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-18 21:34:35 -0600

    Exercise 2 is complete.

---
+++ Python/ex2.py
@@ -0,0 +1,17 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Exercise 2: Comments and Pound Characters
+# Date:     February 18, 2019
+#
+
+# A comment, this is so you can ready your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+# end of script #
