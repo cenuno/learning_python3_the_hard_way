db205990b8785d2b05f49ec898e31cb7db37fd89
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-19 23:03:54 -0600

    Exercise 5 is complete.

---
+++ Python/ex5.py
@@ -0,0 +1,34 @@
+#
+# Author:       Cristian E. Nuno
+# Purpose:      Exercise 5: More Variables and Printing
+# Date:         February 19, 2019
+#
+
+# introduction to the concept of 'format string'
+#
+# you embed variables inside a string by using a special {} sequence and
+# then put the variable you want inside the {} characters.
+#
+# you must also start the string with the letter 'f' for "format"
+# as in f"Hello {somevar}"
+#
+my_name = 'Zedd A. Shaw'
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
+
+# end of script #
