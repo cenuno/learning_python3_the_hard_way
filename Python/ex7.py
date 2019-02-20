f477c66c59512187efa01ccc9b12c071ebdb459a
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-19 23:25:41 -0600

    Exercise 7 is complete.

---
+++ Python/ex7.py
@@ -0,0 +1,40 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Exercise 7: More Printing
+# Date:     February 19, 2019
+#
+
+# the beginning of a bunch of exercises where I just type code in and
+# make it run.
+
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10) # repeats the '.' character 10 times
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end. try removing it to see what happens
+# note: with end = ' ', the second print statement is concatendated onto
+#       the first print statement.
+#       without it, the two statements are printed on separate lines
+print(end1 + end2 + end3 + end4 + end5 + end6, end = ' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+
+# great note at the end:
+# programmers are like magicians who fool everyone into thinking they are
+# perfect and never wrong, but it's all an act.
+# They make mistakes all the time.
+
+# end of script #
