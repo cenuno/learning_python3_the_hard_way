3b30dbfb0997befc0c0bdc5789be1c4e4bb16c46
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-21 20:03:16 -0600

    Exercise 13 is complete.

---
+++ Python/ex13.py
@@ -0,0 +1,19 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Exercise 13: Parameters, UNpacking, Variables
+# Date:     February 21, 2019
+#
+
+# import necessary modules ----
+from sys import argv
+# whatever is in argv, unpack it and assign it
+# to all of these variables on the left in order
+script, first, second = argv
+third = input("What is your thid variable? ")
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+
+# end of script #
