ca9ff6053797503b5d2ef68a238ddcb4bf43db93
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-21 19:42:54 -0600

    Exercise 11 is complete.

---
+++ Python/ex11.py
@@ -0,0 +1,20 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Exercise 11: Asking Questions
+# Date:     February 21, 2019
+#
+# Most of what software does is the following:
+# 1. Takes some kind of input from a person.
+# 2. Changes it.
+# 3. Prints out something to show how it changed.
+#
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print("How much do you weight?", end = ' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# end of script #
