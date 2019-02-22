4d43fd9590099af5ffe5503d9acb76d6a0e30342
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-21 19:49:27 -0600

    Exercise 12 is complete.

---
+++ Python/ex12.py
@@ -0,0 +1,14 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Exercise 12: Prompting People
+# Date:     February 21, 2019
+#
+# for input you can also put a prompt to show
+# a person so she knows what to type
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+# end of script #
