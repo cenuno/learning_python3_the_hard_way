9ab86060268aabb7a0adc55080258a1b2785594b
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-21 19:34:27 -0600

    Exercise 10 is complete.

---
+++ Python/ex10.py
@@ -0,0 +1,26 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Exercise 10: What was that?
+# Date:     February 21, 2019
+#
+
+# an importan escape sequence is to escape
+# a single-quote (') or double-quote ("")
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+# end of script #
