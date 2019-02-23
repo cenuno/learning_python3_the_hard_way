bd4c6420b6905113b962d1572e15cb33bdcdc232
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-22 20:24:36 -0600

    Removed '.' from string

--- Python/ex14.py
+++ Python/ex14.py
@@ -12,7 +12,7 @@ prompt = '> '
 
 print(f"Hi {user_name}, I'm the {script} script.")
 print("I'd like to ask you a few questions.")
-print(f"Do you like me {user_name}?.")
+print(f"Do you like me {user_name}?")
 likes = input(prompt)
 
 print(f"Where do you live {user_name}?")
