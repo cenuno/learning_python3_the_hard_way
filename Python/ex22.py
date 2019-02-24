060ac736e7014c84cec90068507c9cc4dcc83f51
cenuno <24255819+cenuno@users.noreply.github.com>
2019-02-23 23:27:34 -0600

    Exercise 22 is complete.

---
+++ Python/ex22.py
@@ -0,0 +1,47 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Exercise 22: What Do You Know So Far?
+# Date:     February 23, 2019
+#
+
+# write down every and symbol that you have used
+# make sure your list is complete
+
+print('''
+* # is known as pound, hash, and octothorpe. It is used for comments.
+* print() is used to print text to console.
+* Store numbers or text in objects called variables. While you can name them
+anything you would like, be sure to spell them with lower case and snake_case.
+* There's a whole host of mathematic characters, such as:
+\t +: plus
+\t -: minus
+\t /: slash (division)
+\t *: asterik (multiplication)
+\t %: percent (modulus)
+\t <: less than
+\t >: greater than
+\t <=: less than or equal
+\t >=: greater than or equal
+* print(f"I have seen over {movies_num} movies.") is called a format string
+because it allows you to embed variables with a string.
+Alternatively, one may use print("I have seen over {} movies".format('9000'))
+to embed values in a string.
+* input() captures user input data. You can nudge users to specify something
+in particular by typing a string inside on input (i.e. input("Number 1:100")).
+* Store many user inputs via argv from sys module. argv will only capture as
+many input as there are variables to store them (i.e. var1, var2 = argv will
+only store two users inputs, no more and no less.)
+* open() opens a file object
+* read() reads a file object
+* readLine() reads a specific line in a text file.
+* seek() moves the file to a specific bye in the file.
+* truncate() shortens the file.
+* close() closes a file object.
+* write() writes a file object.
+* def is used to write a function, which is a 'mini-script' used to perform an
+operation within a larger script. Function names are lower case and snake_case.
+The arguments are contained within a parentheses, which is followed by a colon.
+The logic within the function is four spaces indented from def.
+''')
+
+# end of script #
