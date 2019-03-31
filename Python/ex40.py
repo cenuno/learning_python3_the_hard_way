59b5c7fa4194f89ab8532c15382c9ebb2dc49d3a
cenuno <24255819+cenuno@users.noreply.github.com>
2019-03-30 21:25:50 -0700

    Exercise 40 is complete

---
+++ Python/ex40.py
@@ -0,0 +1,27 @@
+#
+# Author:   Cristian E. Nuno
+# Purpose:  Exercise 40: Modules, Classes, and Objects
+# Date:     March 30, 2019
+#
+
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+happy_bday = Song(["Happy birthday to you",
+                    "I don't want to get sued",
+                    "So I'll top right there."])
+
+bulls_on_parade = Song(["They rally around tha family",
+                        "With pockets full of shells"])
+
+happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
+
+# end of script #
